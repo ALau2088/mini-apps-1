044c58a788bdaf25a875e24c0997fe9d973edc3d
Andrew Lau <alau12004@yahoo.com>
2019-08-12 14:27:25 -0700

    alternate between X and O

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,9 +1,10 @@
 alert('Let\'s play tic-tac-toe!')
 
+// Possible ways to win
 var possibleWins = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
 
 var x = [];
-var y = []
+var y = [];
 
 document.getElementById("1").addEventListener("click", onClick);
 document.getElementById("2").addEventListener("click", onClick);
@@ -17,13 +18,20 @@ document.getElementById("9").addEventListener("click", onClick);
 
 function onClick(e){
   // x goes first
-  current = 'x'
-  // Add innerhtml x or o
-  e.target.innerHTML= current;
+    // Add innerhtml x or o
+  if (document.getElementById("tracker").value){
+    e.target.innerHTML = 'O'
+  } else {
+    e.target.innerHTML = 'X'
+  }
+
+
+  // Set div #tracker to keep track of value
+  document.getElementById("tracker").value= !document.getElementById("tracker").value
   // Add value to x or o array
   // Check each possible win in values
     // yes alert win
-    // no change current to !current 
+    // no change current to !current
 }
 
 // TOD0: reset button
