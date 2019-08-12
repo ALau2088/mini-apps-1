7b9ad49b7c174b854bb1bff6d4d5fdf80cf053fb
Andrew Lau <alau12004@yahoo.com>
2019-08-12 12:02:38 -0700

    set up tic-tac-toe board to add x to innerHTML

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,30 @@
+alert('Let\'s play tic-tac-toe!')
 
+var possibleWins = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
 
+var x = [];
+var y = []
+
+document.getElementById("1").addEventListener("click", onClick);
+document.getElementById("2").addEventListener("click", onClick);
+document.getElementById("3").addEventListener("click", onClick);
+document.getElementById("4").addEventListener("click", onClick);
+document.getElementById("5").addEventListener("click", onClick);
+document.getElementById("6").addEventListener("click", onClick);
+document.getElementById("7").addEventListener("click", onClick);
+document.getElementById("8").addEventListener("click", onClick);
+document.getElementById("9").addEventListener("click", onClick);
+
+function onClick(e){
+  // x goes first
+  current = 'x'
+  // Add innerhtml x or o
+  e.target.innerHTML= current;
+  // Add value to x or o array
+  // Check each possible win in values
+    // yes alert win
+    // no change current to !current 
+}
+
+// TOD0: reset button
+// clear inner html
