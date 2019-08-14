5b185754efbc5c30083e2a00407c2b8d21c0481e
Andrew Lau <alau12004@yahoo.com>
2019-08-14 15:03:35 -0700

    setup list of React components

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,6 +1,43 @@
+//  App = () => {
+//   return(
+//     <h1>Hello World!</h1>
+//   )
+// }
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      form: 'home' //keeps track of current form: home/f1/f2/f3/confirmation
+    }
+  }
+  render() {
+    if(this.state.form === 'home'){
+      return (
+        <h1>home</h1>
+      )
+    } else if (this.state.form === 'f1'){
+     return (
+        <h1>f1</h1>
+     )
+    } else if (this.state.form === 'f2') {
+      return (
+      <h1>f2</h1>
+      )
+    } else if (this.state.form === 'f3') {
+      return (
+      <h1>f3</h1>
+      )
+    } else if (this.state.form ==='confirmation'){
+      return (
+      <h1>confirmation</h1>
+      )
+    }
+}
+}
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App/>,
   document.getElementById('root')
 );
 
